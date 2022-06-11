cc8b3d42acd3648d2f939834100577083730b1cb
Igor M <russo.programmisto@gmail.com>
2022-06-11 17:45:33 +0300

    Updated dist folder

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,9 @@
-import { StatePostalCode } from "./types";
 export default class ZipMonster {
-    static getStateCodeFromZipCode: (zipCode: string) => StatePostalCode | undefined;
+    static getZipData: (zipCode: string) => {
+        stateCode: import("./types").StatePostalCode;
+        city: string;
+        county: string;
+        zip: string;
+        zipType: "Non-Unique" | "PO Box";
+    } | undefined;
 }
--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,9 @@ const store_1 = __importDefault(require("./data/store"));
 class ZipMonster {
 }
 exports.default = ZipMonster;
-ZipMonster.getStateCodeFromZipCode = (zipCode) => {
-    var _a;
-    return (_a = store_1.default.zipCityStateList.find(state => state.zip === zipCode)) === null || _a === void 0 ? void 0 : _a.stateCode;
+ZipMonster.getZipData = (zipCode) => {
+    const zipCityState = store_1.default.zipCityStateList.find(state => state.zip === zipCode);
+    if (zipCityState) {
+        return Object.assign({}, zipCityState);
+    }
 };
