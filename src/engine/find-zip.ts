04359680bd34dca3605d86cdd1a12a9f41e20db7
Igor M <russo.programmisto@gmail.com>
2022-09-05 19:47:56 +0300

    New implementation for findZip method

--- src/engine/find-zip.ts
+++ src/engine/find-zip.ts
@@ -1,30 +1,184 @@
 import store from "../data/store";
 import { ZipInformation } from "../data/models/zip-information";
+import { ZipType } from "../types/zip-type";
+import { StatePostalCode } from "../types/state-postal-code";
 
 export const findZip = (filter?: ZipFilter, source?: ZipInformation[]): ZipInformation[] => {
-    let allZipCodes = source ?? store.zipCodesInformation;
+    let result = Array.from(
+        source ?? store.zipCodesInformation
+    );
 
-    if (isZipFilterByZipCode(filter)) {
-        return allZipCodes.filter(el => el.zip.toLowerCase() === filter.byZip.toLowerCase());
-    } else if (isZipFilterByCity(filter)) {
-        return allZipCodes.filter(el => el.city.toLowerCase() === filter.byCity.toLowerCase());
-    } else if (isZipFilterByCounty(filter)) {
-        return allZipCodes.filter(el => el.county.toLowerCase() === filter.byCounty.toLowerCase());
-    } else if (isZipFilterByStateCode(filter)) {
-        return allZipCodes.filter(el => el.stateCode.toLowerCase() === filter.byStateCode.toLowerCase());
-    } else {
-        return allZipCodes;
+    if (!filter) {
+        return result;
     }
+
+    const applyStringFilter = (filter: string, stringFieldName: keyof ZipInformation) => {
+        const searchValue = filter;
+        result = result.filter(el => el[stringFieldName] === searchValue);
+    }
+
+    const applyStringFlexibleFilter = (filter: ZipSearch_StringFlexibleFilter, stringFieldName: keyof ZipInformation) => {
+        const searchValue = filter.caseSensitive
+            ? filter.value
+            : filter.value.toLowerCase();
+        result = result.filter(el => {
+            const valueFromElement = filter.caseSensitive
+                ? el[stringFieldName] as string
+                : (el[stringFieldName] as string).toLowerCase();
+            return filter.wholeMatch
+                ? valueFromElement === searchValue
+                : valueFromElement.includes(searchValue);
+        });
+    }
+
+    const applyRegularExpressionFilter = (filter: RegExp, stringFieldName: keyof ZipInformation) => {
+        const regularExpression = filter;
+        result = result.filter(el => regularExpression.test(el[stringFieldName] as string));
+    }
+
+    const applyLocationFlexibleFilter = (filter: ZipSearch_LocationFlexibleFilter, locationFieldName: keyof ZipInformation) => {
+        const latitudeRequirement = filter.latitude;
+        const longitudeRequirement = filter.longitude;
+
+        if (latitudeRequirement || longitudeRequirement) {
+            result = result.filter(el => el.location !== undefined);
+
+            if (latitudeRequirement) {
+                result = result.filter(el => {
+                    switch (latitudeRequirement.direction) {
+                        case "to-north": {
+                            return el.location!.latitude > latitudeRequirement.value;
+                        }
+                        case "to-south": {
+                            return el.location!.latitude < latitudeRequirement.value;
+                        }
+                        case "here": {
+                            return el.location!.latitude === latitudeRequirement.value;
+                        }
+                    }
+                });
+            }
+
+            if (longitudeRequirement) {
+                result = result.filter(el => {
+                    switch (longitudeRequirement.direction) {
+                        case "to-east": {
+                            return el.location!.longitude > longitudeRequirement.value;
+                        }
+                        case "to-west": {
+                            return el.location!.longitude < longitudeRequirement.value;
+                        }
+                        case "here": {
+                            return el.location!.longitude === longitudeRequirement.value;
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    if (filter.zip) {
+        if (typeof filter.zip === "string") {
+            applyStringFilter(
+                filter.zip,
+                "zip"
+            );
+        } else if (is_zipSearch_stringFlexibleFilter(filter.zip)) {
+            applyStringFlexibleFilter(
+                filter.zip,
+                "zip"
+            );
+        } else if (filter.zip instanceof RegExp) {
+            applyRegularExpressionFilter(
+                filter.zip,
+                "zip"
+            );
+        }
+    }
+
+    if (filter.stateCode) {
+        if (typeof filter.stateCode === "string") {
+            applyStringFilter(
+                filter.stateCode,
+                "stateCode"
+            );
+        } else if (is_zipSearch_stringFlexibleFilter(filter.stateCode)) {
+            applyStringFlexibleFilter(
+                filter.stateCode,
+                "stateCode"
+            );
+        } else if (filter.stateCode instanceof RegExp) {
+            applyRegularExpressionFilter(
+                filter.stateCode,
+                "stateCode"
+            );
+        }
+    }
+
+    if (filter.county) {
+        if (typeof filter.county === "string") {
+            applyStringFilter(
+                filter.county,
+                "county"
+            );
+        } else if (is_zipSearch_stringFlexibleFilter(filter.county)) {
+            applyStringFlexibleFilter(
+                filter.county,
+                "county"
+            );
+        } else if (filter.county instanceof RegExp) {
+            applyRegularExpressionFilter(
+                filter.county,
+                "county"
+            );
+        }
+    }
+
+    if (filter.city) {
+        if (typeof filter.city === "string") {
+            applyStringFilter(
+                filter.city,
+                "city"
+            );
+        } else if (is_zipSearch_stringFlexibleFilter(filter.city)) {
+            applyStringFlexibleFilter(
+                filter.city,
+                "city"
+            );
+        } else if (filter.city instanceof RegExp) {
+            applyRegularExpressionFilter(
+                filter.city,
+                "city"
+            );
+        }
+    }
+
+    if (filter.zipType) {
+        result = result.filter(el => el.zipType === filter.zipType);
+    }
+
+    if (filter.location) {
+        applyLocationFlexibleFilter(
+            filter.location,
+            "location"
+        );
+    }
+
+    return result;
 }
 
-export interface ZipFilterByZipCode {
-    byZip: string
+export interface ZipSearch_StringFlexibleFilter {
+    value: string,
+    caseSensitive: boolean,
+    wholeMatch: boolean
 }
 
-const isZipFilterByZipCode = (obj: any): obj is ZipFilterByZipCode => {
+const is_zipSearch_stringFlexibleFilter = (obj: any): obj is ZipSearch_StringFlexibleFilter => {
     if (typeof obj === "object") {
         const requirements = [
-            typeof obj["byZip"] === "string"
+            typeof obj["value"] === "string",
+            typeof obj["caseSensitive"] === "boolean",
+            typeof obj["wholeMatch"] === "boolean"
         ];
         return !requirements.includes(false);
     } else {
@@ -32,49 +186,22 @@ const isZipFilterByZipCode = (obj: any): obj is ZipFilterByZipCode => {
     }
 }
 
-export interface ZipFilterByCity {
-    byCity: string
-}
-
-const isZipFilterByCity = (obj: any): obj is ZipFilterByCity => {
-    if (typeof obj === "object") {
-        const requirements = [
-            typeof obj["byCity"] === "string"
-        ];
-        return !requirements.includes(false);
-    } else {
-        return false;
+export interface ZipSearch_LocationFlexibleFilter {
+    latitude?: {
+        value: number,
+        direction: "to-north" | "to-south" | "here"
+    },
+    longitude?: {
+        value: number,
+        direction: "to-east" | "to-west" | "here"
     }
 }
 
-export interface ZipFilterByCounty {
-    byCounty: string
+export interface ZipFilter {
+    zip?: string | ZipSearch_StringFlexibleFilter | RegExp,
+    zipType?: ZipType,
+    stateCode?: StatePostalCode | ZipSearch_StringFlexibleFilter | RegExp,
+    county?: string | ZipSearch_StringFlexibleFilter | RegExp,
+    city?: string | ZipSearch_StringFlexibleFilter | RegExp,
+    location?: ZipSearch_LocationFlexibleFilter
 }
-
-const isZipFilterByCounty = (obj: any): obj is ZipFilterByCounty => {
-    if (typeof obj === "object") {
-        const requirements = [
-            typeof obj["byCounty"] === "string"
-        ];
-        return !requirements.includes(false);
-    } else {
-        return false;
-    }
-}
-
-export interface ZipFilterByStateCode {
-    byStateCode: string
-}
-
-const isZipFilterByStateCode = (obj: any): obj is ZipFilterByStateCode => {
-    if (typeof obj === "object") {
-        const requirements = [
-            typeof obj["byStateCode"] === "string"
-        ];
-        return !requirements.includes(false);
-    } else {
-        return false;
-    }
-}
-
-export type ZipFilter = ZipFilterByZipCode | ZipFilterByCity | ZipFilterByCounty | ZipFilterByStateCode;
